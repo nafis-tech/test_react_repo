3650436d490c7d4664db4a7371d3cc280a8605df
nafis <nafiezlela9@gmail.com>
2023-07-19 15:21:14 +0700

    Updates

---
+++ precache-manifest.b698f8dd156a4d4454c5b3f08b78c0f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa9a2006e89e96b264b7",
+    "url": "/test_react_repo/static/css/main.dfb0f9be.chunk.css"
+  },
+  {
+    "revision": "aa9a2006e89e96b264b7",
+    "url": "/test_react_repo/static/js/main.aa9a2006.chunk.js"
+  },
+  {
+    "revision": "ae3ce3c0c4c10ebed2f3",
+    "url": "/test_react_repo/static/css/1.6278d0d3.chunk.css"
+  },
+  {
+    "revision": "ae3ce3c0c4c10ebed2f3",
+    "url": "/test_react_repo/static/js/1.ae3ce3c0.chunk.js"
+  },
+  {
+    "revision": "371f7bc07cb39967094b",
+    "url": "/test_react_repo/static/js/runtime~main.371f7bc0.js"
+  },
+  {
+    "revision": "f8e0bd1ba4dad5149fc1dd4cad20d52b",
+    "url": "/test_react_repo/index.html"
+  }
+];
